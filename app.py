70c983a48ce99b3d2232568d14298b5f0150cd24
Nick Benjamin <NBLaptop@Nicks-MacBook-Air.local>
2019-07-17 11:46:06 -0400

    possibly done

---
+++ app.py
@@ -0,0 +1,111 @@
+#So so many dependencies...
+from flask import Flask, jsonify
+from sqlalchemy import create_engine, inspect, func
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+import pandas as pd
+import datetime as dt
+from datetime import date
+import numpy as np
+
+#Starting Flask
+app = Flask(__name__)
+
+#Connecting the data
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+conn = engine.connect()
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables, various other connection magic
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+#default page
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Hawaii Weather API!<br>"
+        f"Available Routes:<br>"
+        f"/api/v1.0/precipitation<br>"
+        f"/api/v1.0/stations<br>"
+        f"/api/v1.0/tobs<br>"
+    )
+
+
+#Precipitation page. Reports rainfall from the last 365 days of data, due to the 
+#high number of stations there are 2,223 things in this report
+@app.route("/api/v1.0/precipitation/")
+def precipitation():
+    last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    last_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    rain = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date > last_year).\
+        order_by(Measurement.date).all()
+    rain_totals = []
+    for result in rain:
+        row = {}
+        row["date"] = rain[0]
+        row["prcp"] = rain[1]
+        rain_totals.append(row)
+
+    return jsonify(rain_totals)
+
+#Station page. Lists the 9 stations.
+@app.route("/api/v1.0/stations")
+def stations():
+    stations_query = session.query(Station.name, Station.station)
+    stations = pd.read_sql(stations_query.statement, stations_query.session.bind)
+    return jsonify(stations.to_dict())
+
+#Returns temperatures from the last 365 days, like the temperature report it has 2,223 items
+@app.route("/api/v1.0/tobs")
+def tobs():
+    last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    last_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    temperature = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date > last_year).\
+        order_by(Measurement.date).all()
+
+    temperature_totals = []
+    for result in temperature:
+        row = {}
+        row["date"] = temperature[0]
+        row["tobs"] = temperature[1]
+        temperature_totals.append(row)
+
+    return jsonify(temperature_totals)
+
+
+#Reports temperatures from <start>
+@app.route("/api/v1.0/<start>")
+def trip1(start):
+    start_date= dt.datetime.strptime(start, '%Y-%m-%d')
+    last_year = dt.timedelta(days=365)
+    start = start_date-last_year
+    end =  dt.date(2017, 8, 23)
+    trip_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    trip = list(np.ravel(trip_data))
+    return jsonify(trip)
+
+#Reports temperatures from <start> to <end>
+@app.route("/api/v1.0/<start>/<end>")
+def trip2(start,end):
+
+  # go back one year from start/end date and get Min/Avg/Max temp     
+    start_date= dt.datetime.strptime(start, '%Y-%m-%d')
+    end_date= dt.datetime.strptime(end,'%Y-%m-%d')
+    last_year = dt.timedelta(days=365)
+    start = start_date-last_year
+    end = end_date-last_year
+    trip_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    trip = list(np.ravel(trip_data))
+    return jsonify(trip)
+
+if __name__ == "__main__":
+    app.run(debug=True)
